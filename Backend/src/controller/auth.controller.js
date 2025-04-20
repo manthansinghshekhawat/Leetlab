e3633bfc7b463adad6096f4280f0a9ff24dc3c22
Manthan Shekhawat <manthanshekhawat@Manthans-MacBook-Air.local>
2025-04-20 07:04:58 +0530

    boiler plate for authentication

---
+++ Backend/src/controller/auth.controller.js
@@ -0,0 +1,5 @@
+
+export const register = async (req, res) => { };
+export const logout = async (req, res) => {};
+export const login = async (req, res) => {};
+export const check = async (req, res) => {};
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello guys welcome to LEETLAB");
+});
+
+app.get
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT} `);
 });
