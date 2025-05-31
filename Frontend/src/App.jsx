9d881c3a2df1a01a6402ba6f3436d8bcf352df1d
Manthan Shekhawat <manthanshekhawat@Manthans-MacBook-Air.local>
2025-05-31 22:27:33 +0530

    frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div className="text-2xl font-bold text-indigo-400 ">
+      <h1>hello there</h1>
+    </div>
+  );
+};
+export default App;
